c1b5253b46b2a1be1428d2cbddbdd614f8fd2c62
saumittras <saumittra.eee@gmail.com>
2024-10-17 04:50:49 +0600

    added state handling code

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Counter from "./Counter";
+
+function App() {
+  function handleClick() {
+    alert("Button Clicked");
+  }
+
+  const clickHandle2 = () => {
+    alert("Button Clicked 2");
+  };
+
+  const addToFive = (num) => {
+    alert(num + 3);
+  };
+  return (
+    <>
+      <Counter></Counter>
+      <h3>React Core Concept 2</h3>
+      <button onClick={handleClick}>Click Me!</button>
+      <button onClick={clickHandle2}>Click Me2!</button>
+      <button onClick={() => alert("Button 3 Clicked")}>Click Me3!</button>
+      <button onClick={() => addToFive(3)}>Vajilla Button</button>
+    </>
+  );
+}
+
+export default App;
